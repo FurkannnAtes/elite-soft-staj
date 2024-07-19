460270cc56705542189299e19f486f45aa0a24eb
FurkannnAtes <aliriza20005@hotmail.com>
2024-07-19 13:28:11 +0300

    update

--- pages/index.jsx
+++ pages/index.jsx
@@ -6,29 +6,16 @@ import React from "react";
 const Home = () => {
   const router = useRouter();
 
-  const goHome = () => {
-    console.log("tıklandı");
-    router.push("/about");
+  const handleGoProfile = () => {
+    router.push("/profile");
   };
 
   return (
     <div>
-      <ul>
-        <li>
-          <Link href="/">Home</Link>
-        </li>
-        <li>
-          <Link href="/about">About Us</Link>
-        </li>
-        <li>
-          <Link href="/profile">Profile</Link>
-        </li>
-      </ul>
-      <div>
-        <Button onClick={goHome} type="primary">
-          Go to About
-        </Button>
-      </div>
+      <Link href={"/about"}>About kismina gidiyor</Link>
+      <Button type="primary" onClick={() => handleGoProfile()}>
+        Diğer Sayfaya Git
+      </Button>
     </div>
   );
 };
